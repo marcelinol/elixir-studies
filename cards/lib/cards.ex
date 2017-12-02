f8b9529cc4ab2a3ea33031ee0acf9a1a632ff6fa
marcelinol <marcelinolucianom@gmail.com>
2017-12-02 15:49:40 -0200

    Create Cards project

---
+++ cards/lib/cards.ex
@@ -0,0 +1,35 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight"]
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
