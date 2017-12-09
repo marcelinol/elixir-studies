d519c13865d481a9237e7357bb5f7f06f36ff50d
marcelinol <marcelinolucianom@gmail.com>
2017-12-09 11:02:35 -0200

    Save deck to a fila and load deck from a file

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -36,4 +36,18 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, ""} -> "Deck is empty"
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, :enoent} -> "File does not exist"
+      {:error, _reason} -> "Something went wrong"
+    end
+  end
 end
